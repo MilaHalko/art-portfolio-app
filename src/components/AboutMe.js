13e910d403d6bbfeffebdde9e6fd95e381660b9f
milka_deluxe <milagalko1@gmail.com>
2024-02-25 22:55:36 +0200

    AboutMe init

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div name='aboutMe' className='w-full h-screen'>
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+
+            </div>
+        </div>
+    );
+};
+
+export default AboutMe;
